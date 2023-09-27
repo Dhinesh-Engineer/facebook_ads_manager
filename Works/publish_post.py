bf8b0dc67ad06e2240c7e96f04a6d76f1426dff0
R. Dhineshkumar B.E CSE <89518390+Dhinesh-Engineer@users.noreply.github.com>
2023-09-27 14:57:08 +0530

    Update publish_post.py

--- Works/publish_post.py
+++ Works/publish_post.py
@@ -3,10 +3,9 @@ import requests
 page_id = input("Enter the Page Id :") 
 page_access_token = input("Enter the access token :")  
 
-# Define the message you want to post
 message = input("Enter the Message to be posted:")
 
-# Create the API endpoint URL
+# API URL
 url = f"https://graph.facebook.com/{page_id}/feed"
 
 # Define the parameters for the POST request
@@ -15,7 +14,7 @@ params = {
     "access_token": page_access_token,
 }
 
-# Send the POST request to post the message to the Page's feed
+# POST request 
 response = requests.post(url, params=params)
 
 # Check the response status code and handle errors
